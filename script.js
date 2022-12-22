cf90f1e0e9f35b34dab0bd1f75122c0dc4999b81
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-12-22 14:12:38 -0300

    using animation for a better user experience examples

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-
-// $('.box').on('click', function(){
-//    $(this).toggleClass('is-paused');
-// });
-// now not using jquery: 
-
-$('html').on('click', function(){
-    $(this).find('body').append('<div class="box"></div>');
-})
+$('.example-1 .button').click(function(){
+    $(this).parents('.example-1').toggleClass('is-transitioned');
+  });
+  
+  $('.example-2 .switch').click(function(){
+    $(this).toggleClass('is-transitioned');
+  });
+  
+  $('.example-3 .btn').click(function(){
+    $(this).parents('.layout').find('.alert').toggleClass('is-open');
+  });
+  
+  $('.example-5 .send').click(function(){
+    $(this).parents('.email').addClass('is-sent');
+    setTimeout(function(){
+      $('.email').removeClass('is-sent');  
+    }, 1800);
+    
+  });
