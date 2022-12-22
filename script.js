0afa4f312c29f2e94d95d67c95821a0cc99457ef
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-12-22 09:40:22 -0300

    adding animation to some div without jquery, just js

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-// $('.trigger').on('click', function(){
-//     $(this).append('<div class="box">')
-// });
 
-$('.box').on('click', function(){
-   $(this).toggleClass('is-paused');
-});
+// $('.box').on('click', function(){
+//    $(this).toggleClass('is-paused');
+// });
+// now not using jquery: 
+
+const boxes = document.querySelectorAll('.box');
+boxes.forEach(function (box){
+    box.addEventListener('click', function(){
+        this.classList.toggle('is-paused');
+    })
+})
